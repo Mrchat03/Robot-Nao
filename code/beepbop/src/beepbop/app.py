4cb49f8c3000dcfabd5a3b986a91f7031d89b813
Eric <ericptrgingras@outlook.com>
2022-11-09 17:02:03 -0500

    Ajout des boîtes dans beeware

--- code/beepbop/src/beepbop/app.py
+++ code/beepbop/src/beepbop/app.py
@@ -9,14 +9,39 @@ from toga.style.pack import COLUMN, ROW
 class BeepBop(toga.App):
 
     def startup(self):
-        """
-        Construct and show the Toga application.
+        #-----------Création des boîtes--------------
+        main_box = toga.Box(style=Pack(direction=ROW))
+        #Trois boîtes principales
+        rgb_box = toga.Box(style=Pack(direction=COLUMN))
+        btn_box = toga.Box(style=Pack(direction=COLUMN))
+        joystick_box = toga.Box(style=Pack(direction=COLUMN))
+        #Les sous-boîtes
+        
+        rgbChoix_box = toga.Box(style=Pack(direction=ROW))
+        rgbRouge_box = toga.Box(style=Pack(direction=ROW))
+        rgbGreen_box = toga.Box(style=Pack(direction=ROW))
+        rgbBlue_box = toga.Box(style=Pack(direction=ROW))
+        
+        btnPos_box = toga.Box(style=Pack(direction=ROW))
+        btnTourne_box = toga.Box(style=Pack(direction=ROW))
+        btnAnim_box = toga.Box(style=Pack(direction=ROW))
+        btnMsg_box = toga.Box(style=Pack(direction=ROW))
+        
+        joystickAvance_box = toga.Box(style=Pack(direction=ROW))
+        joystickCote_box = toga.Box(style=Pack(direction=ROW))
+        joystickRecul_box = toga.Box(style=Pack(direction=ROW))
+        
+        #Création des labels
+        rouge_label = toga.Label('R')
+        vert_label = toga.Label('G')
+        bleu_label = toga.Label('B')
+       
+        
+        
 
-        Usually, you would add your application to a main content box.
-        We then create a main window (with a name matching the app), and
-        show the main window.
-        """
-        main_box = toga.Box()
+
+
+
 
         self.main_window = toga.MainWindow(title=self.formal_name)
         self.main_window.content = main_box
