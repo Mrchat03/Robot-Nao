7eb5f52a3988b19dbabec189a6b3944bdff1db4d
Nath <34311270+Mrchat03@users.noreply.github.com>
2022-11-09 15:18:13 -0500

    Changement code app.py

--- code/beepbop/src/beepbop/app.py
+++ code/beepbop/src/beepbop/app.py
@@ -1,56 +1,27 @@
+"""
+Application pour piloter un Robot Nao
+"""
 import toga
-from toga.style.pack import CENTER, COLUMN, ROW, Pack
+from toga.style import Pack
+from toga.style.pack import COLUMN, ROW
 
 
-class Graze(toga.App):
+class BeepBop(toga.App):
+
     def startup(self):
-        self.main_window = toga.MainWindow(title=self.name)
+        """
+        Construct and show the Toga application.
 
-        self.webview = toga.WebView(
-            on_webview_load=self.on_webview_loaded, style=Pack(flex=1)
-        )
-        self.url_input = toga.TextInput(
-            value="https://beeware.org/", style=Pack(flex=1)
-        )
+        Usually, you would add your application to a main content box.
+        We then create a main window (with a name matching the app), and
+        show the main window.
+        """
+        main_box = toga.Box()
 
-        box = toga.Box(
-            children=[
-                toga.Box(
-                    children=[
-                        self.url_input,
-                        toga.Button(
-                            "Go",
-                            on_press=self.load_page,
-                            style=Pack(width=50, padding_left=5),
-                        ),
-                    ],
-                    style=Pack(
-                        direction=ROW,
-                        alignment=CENTER,
-                        padding=5,
-                    ),
-                ),
-                self.webview,
-            ],
-            style=Pack(direction=COLUMN),
-        )
-
-        self.main_window.content = box
-        self.webview.url = self.url_input.value
-
-        # Show the main window
+        self.main_window = toga.MainWindow(title=self.formal_name)
+        self.main_window.content = main_box
         self.main_window.show()
-
-    def load_page(self, widget):
-        self.webview.url = self.url_input.value
-
-    def on_webview_loaded(self, widget):
-        self.url_input.value = self.webview.url
 
 
 def main():
-    return Graze("Graze", "org.beeware.graze")
-
-
-if __name__ == "__main__":
-    main().main_loop()
+    return BeepBop()
