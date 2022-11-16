611f61be003b5530ba27fa9a119264fdb7b4a612
Eric <ericptrgingras@outlook.com>
2022-11-16 15:45:31 -0500

    Ajout des icones pour les boutons Oreilles et Yeux

--- code/beepbop/src/beepbop/app.py
+++ code/beepbop/src/beepbop/app.py
@@ -4,12 +4,16 @@ Application pour piloter un Robot Nao
 import toga
 import socket
 from toga.style import Pack
-from toga.style.pack import COLUMN, ROW
+from toga.style.pack import COLUMN, ROW, CENTER, JUSTIFY
 
 
 class BeepBop(toga.App):
     #UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
     def startup(self):
+        oreille_icon = "icon/oreille.png"
+        
+        
+        
         #-----------Création des boîtes--------------
         
         #Les sous-boîtes
@@ -27,45 +31,49 @@ class BeepBop(toga.App):
         joystickCote_box = toga.Box(style=Pack(direction=ROW))
         joystickRecul_box = toga.Box(style=Pack(direction=ROW))
         
-
         rgb_box = toga.Box(style=Pack(direction=COLUMN), children=[rgbChoix_box, rgbRouge_box, rgbVert_box, rgbBleu_box])
         btn_box = toga.Box(style=Pack(direction=COLUMN), children=[btnPos_box, btnTourne_box, btnAnim_box, btnMsg_box])
         joystick_box = toga.Box(style=Pack(direction=COLUMN), children=[joystickAvance_box, joystickCote_box, joystickRecul_box])
         main_box = toga.Box(style=Pack(direction=ROW, padding=5), children=[rgb_box])
-   
-        #-----Création des boutons-----
-        btn_rgbOreilles = toga.Button(
-            'O',
-            style=Pack(padding=5)
-        )
         
-        btn_rgbYeux = toga.Button(
-            'Y',
-            style=Pack(padding=5)
-        )
+        
+        #-----Création des boutons-----
+
+        
+        btn_Oreille = toga.Button(text = "👀", style=Pack(font_size=25, width=50, padding_right=10))
+        btn_Yeux = toga.Button(text="👂", style=Pack(font_size=25, width=50))
+        
+        
+        #-----Création des sliders-----
+        slider_rouge = toga.Slider()
+        slider_vert = toga.Slider()
+        slider_bleu = toga.Slider()
    
         #-----Création des labels------
-        lbl_rouge = toga.Label('R', style=Pack(padding=(5)))
-        lbl_vert = toga.Label('G', style=Pack(padding=(5)))
-        lbl_bleu = toga.Label('B', style=Pack(padding=(5)))
+        lbl_rouge = toga.Label('R')
+        lbl_vert = toga.Label('G')
+        lbl_bleu = toga.Label('B')
+        
 
-        rgbChoix_box.add(btn_rgbOreilles)
-        rgbChoix_box.add(btn_rgbYeux)
+        #-----Ajout des items aux boîtes rgb-----
+        rgbChoix_box.add(btn_Oreille)
+        rgbChoix_box.add(btn_Yeux)
         rgbRouge_box.add(lbl_rouge)
+        rgbRouge_box.add(slider_rouge)
         rgbVert_box.add(lbl_vert)
+        rgbVert_box.add(slider_vert)
         rgbBleu_box.add(lbl_bleu)
-   
-
-        #Création des labels
-        rouge_label = toga.Label('R')
-        vert_label = toga.Label('G')
-        bleu_label = toga.Label('B')
-
-
+        rgbBleu_box.add(slider_bleu)
+        
+        #Ajout des items aux boîtes btn-----
+        
+        
         self.main_window = toga.MainWindow(title=self.formal_name)
         self.main_window.content = main_box
         self.main_window.show()
 
+def oreille_action(widget):
+    print("oreille")
 
        
     ### Fonction pour configurer l'UDP et envoyer des messages###   
