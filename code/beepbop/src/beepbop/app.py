c0608dd203a03b578c2a8b1b25c17f8cbd5d244a
Jabooby <65316204+Jabooby@users.noreply.github.com>
2022-12-20 13:28:47 -0500

    Version final application beepbop

--- code/beepbop/src/beepbop/app.py
+++ code/beepbop/src/beepbop/app.py
@@ -4,27 +4,25 @@
 @date   6 decembre 2022
 @brief  Application pour piloter un Robot Nao
 @version 1.0
-
 Environnement: Visual Studio Code
 """
 import toga
 import sys
 import time
 import socket
+import threading
+import logging
 from toga.colors import RED, GREEN, BLUE, rgb
 from toga.style import Pack
 from toga.style.pack import COLUMN, ROW
 
 class BeepBop(toga.App):
-    address = ""
-    UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-
+    
     def startup(self):
-
+        
         #-----------Création des boîtes--------------
         
-        #Les sous-boîtes
-        
+        #Les sous-boîte
         btnPos_box = toga.Box(style=Pack(direction=ROW))
         btnTourne_box = toga.Box(style=Pack(direction=ROW))
         btnAnim_box = toga.Box(style=Pack(direction=ROW))
@@ -37,7 +35,8 @@ class BeepBop(toga.App):
         connexion_box = toga.Box(style=Pack(direction=ROW))
         
         #Les boîtes principales
-        btn_box = toga.Box(style=Pack(direction=COLUMN, padding_right=20, padding_left=50), children=[btnPos_box, btnTourne_box, btnAnim_box, btnMsg_box])
+        
+        btn_box = toga.Box(style=Pack(direction=COLUMN, padding_left=20), children=[btnPos_box, btnTourne_box, btnAnim_box, btnMsg_box])
         joystick_box = toga.Box(style=Pack(direction=COLUMN), children=[joystickAvance_box, joystickCote_box, joystickRecul_box])
         main_box = toga.Box(style=Pack(direction=ROW, padding=5), children=[btn_box, joystick_box, connexion_box])
         
@@ -49,18 +48,23 @@ class BeepBop(toga.App):
         btn_tourneDroite = toga.Button(text="↷", style=Pack(font_size=25, width=50), on_press=self.tourneDroite_action)
         btn_tourne180 = toga.Button(text="⟲", style=Pack(font_size=25, width=50), on_press=self.tourne180_action)
         btn_animation = toga.Button(text="🕺", style=Pack(font_size=25, width=50), on_press=self.animation_action)
-        btn_envoie = toga.Button(text="💬", style=Pack(font_size=15, width=50), on_press=self.parle_action)
+        btn_envoie = toga.Button(text="⏩", style=Pack(font_size=15, width=50), on_press=self.parle_action)
         btn_avance = toga.Button(text="🡹", style=Pack(font_size=25, width=50, padding_top=20, padding_left=50), on_press=self.avance_action)
         btn_gauche = toga.Button(text="🡸", style=Pack(font_size=25, width=50, padding_right=50), on_press=self.gauche_action)
         btn_droite = toga.Button(text="🡺", style=Pack(font_size=25, width=50), on_press=self.droite_action)
         btn_recul = toga.Button(text="🡻", style=Pack(font_size=25, width=50, padding_left = 50), on_press=self.recul_action)
         btn_connexion = toga.Button(text="📶", style=Pack(font_size=25, width=50), on_press=self.connect_action)
-        btn_stop = toga.Button(text="❌", style=Pack(font_size=25, width=50), on_press=self.animation_stop)
         
+        #-----Création des sliders-----
+
         
-        #-----Création du textbox-----
+        #-----Ajout du textbox-----
         self.txt_message = toga.TextInput(style=Pack(padding_top=12))
-        
+   
+        #-----Création des labels------
+
+        #-----Ajout des items aux boîtes rgb-----
+
         #Ajout des items aux boîtes btn-----
         btnPos_box.add(btn_debout)
         btnPos_box.add(btn_assis)
@@ -68,7 +72,6 @@ class BeepBop(toga.App):
         btnTourne_box.add(btn_tourneDroite)
         btnAnim_box.add(btn_tourne180)
         btnAnim_box.add(btn_animation)
-        btnAnim_box.add(btn_stop)
         btnMsg_box.add(self.txt_message)
         btnMsg_box.add(btn_envoie)
     
@@ -81,20 +84,20 @@ class BeepBop(toga.App):
         connexion_box.add(btn_connexion)
         
         
-        
         self.main_window = toga.MainWindow(title=self.formal_name)
         self.main_window.content = main_box
         self.main_window.show()
-    
-        
+
     def connect_action(self, widget):
         self.UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
         self.address = ("0", 0)
-        self.configUDPESP()
+        self.x = threading.Thread(target = self.configUDPESP)
+        self.x.start()
+        #self.configUDPESP()
         
         widget.enabled = False
 
-        
+  
     #Place le robot en position debout
     def debout_action(self, widget):
         msg = """{"animation":{"topic":"zbos/motion/animation/run","payload":{"type":"Posture","animationId":"Stand"}}}"""
@@ -109,44 +112,39 @@ class BeepBop(toga.App):
 
     #Tourne le robot vers la gauche de 15 degrés
     def tourneGauche_action(self, widget):
-        msg = """{"movement":{"topic":"zbos/motion/control/movement","payload":{"angle":{"radian":2.2448158576504826,"degree":128.61847442741285},"force":100}}}"""
+        msg = """{"movement":{"topic":"zbos/motion/control/movement","payload":{"yaw": 0,"pitch": 0,"angle":{"degree": 90},"force": 50,"distance": 0.1,"relative_rotation": -45}}}"""
         self.UDPClientSocket.sendto(msg.encode('utf-8'), self.address)
         print("tourneGauche  //  ", msg)
         msg = """{"movement":{"topic":"zbos/motion/control/movement", "payload":{"yaw": 0,"pitch": 0,"angle": {"degree": 0},"force": 0,"distance": 0,"relative_rotation": 0}}}"""
-        time.sleep(3.5)
+        time.sleep(5)
         self.UDPClientSocket.sendto(msg.encode('utf-8'), self.address)
         print("stop   //", msg)
       
     #Tourne le robot vers la droite de 15 degrés  
     def tourneDroite_action(self, widget):
-        msg = """{"movement":{"topic":"zbos/motion/control/movement","payload":{"angle":{"radian":0.6939980604270402,"degree":39.763159852734475},"force":100}}}"""
+        msg = """{"movement":{"topic":"zbos/motion/control/movement","payload":{"yaw": 0,"pitch": 0,"angle": {"degree": 90},"force": 50,"distance": 0.1,"relative_rotation": 45}}}"""
         self.UDPClientSocket.sendto(msg.encode('utf-8'), self.address)
         print("tourneDroite  //  ", msg)
         msg = """{"movement":{"topic":"zbos/motion/control/movement", "payload":{"yaw": 0,"pitch": 0,"angle": {"degree": 0},"force": 0,"distance": 0,"relative_rotation": 0}}}"""
-        time.sleep(3.5)
+        time.sleep(5)
         self.UDPClientSocket.sendto(msg.encode('utf-8'), self.address)
         print("stop   //", msg)
         
     #Tourne le robot 180 degrés
     def tourne180_action(self, widget):
-        msg = """{"movement":{"topic":"zbos/motion/control/movement","payload":{"angle":{"radian":0.6939980604270402,"degree":39.763159852734475},"force":100}}}"""
+        msg = """{"movement":{"topic":"zbos/motion/control/movement","payload":{"yaw": 0,"pitch": 0,"angle": {"degree": 0},"force": 50,"distance": 0,"relative_rotation": 180}}}"""
         self.UDPClientSocket.sendto(msg.encode('utf-8'), self.address)
         print("tourne180  //  ", msg)
         msg = """{"movement":{"topic":"zbos/motion/control/movement", "payload":{"yaw": 0,"pitch": 0,"angle": {"degree": 0},"force": 0,"distance": 0,"relative_rotation": 0}}}"""
-        time.sleep(8)
+        time.sleep(5)
         self.UDPClientSocket.sendto(msg.encode('utf-8'), self.address)
         print("stop   //", msg)
         
     #Fonction pour faire danser le robot
     def animation_action(self, widget):
-        msg = """{"dance":{"topic":"zbos/motion/dance/start", "payload":"chickendance/chickendance"}}""" 
+        msg = """{"dance":{"topic":"zbos/motion/animation/run","payload":{"type":"Posture","animationId":"taichisit/taichisit"}}}"""
         self.UDPClientSocket.sendto(msg.encode('utf-8'), self.address)
         print("animation  //  ", msg)
-        
-    def animation_stop(self, widget):
-        msg = """{"dance":{"topic":"zbos/motion/dance/stop"}}"""
-        self.UDPClientSocket.sendto(msg.encode('utf-8'), self.address)
-        print("stop  //  ", msg)
     
     #Avance le robot de quelques pas
     def avance_action(self, widget):
@@ -191,15 +189,10 @@ class BeepBop(toga.App):
     #Le robot dit ce qui a été saisi dans le textbox
     def parle_action(self, widget):
         #Je vais faire un pas", "language": "fr-FR", "volume": 50}
-        if "mqtt set " in self.txt_message.value:
+        if "ip set " in self.txt_message.value:
             index = self.txt_message.value.find('set') + 4
-            mqtt = self.txt_message.value[index:len(self.txt_message.value)]
-            msg = """{"commande":{"mqtt":{"set":""" +'"' + mqtt + """"}}}"""
-        elif "wifi set" in self.txt_message.value:
-            index = self.txt_message.value.find('set') + 4
-            ssidPwd = self.txt_message.value[index:len(self.txt_message.value)]
-            infoWifi = ssidPwd.split(' ')
-            msg = """{"commande":{"wifi":{"set":{"ssid":""" + '"' + infoWifi[0] + """", "pwd":""" + '"' + infoWifi[1] + """"}}}}"""
+            ip = self.txt_message.value[index:len(self.txt_message.value)]
+            msg = """{"commande":{"ip":{"set":""" +'"' + ip + """"}}}"""
         else:
             msg = """{"dialog":{"topic":"zbos/dialog/set","payload":{"message":""" + '"' + self.txt_message.value + """", "language": "fr-FR", "volume": 50}}}"""
         
@@ -207,36 +200,34 @@ class BeepBop(toga.App):
         print("parle  //  ", msg)
         
     def configUDPESP(self):
-        ip = ""
-        if len(sys.argv) == 3:
-        # Get "IP address of Server" and also the "port number" from
-        #argument 1 and argument 2
-            ip = sys.argv[1]
-        recu = True
+        hostname = socket.gethostname()
+        ip = get_private_ip()
+
         print(ip)
+        logging.info("configUDPESP")
         #Initiation de UDP
-        self.UDPClientSocket.bind(initUDP(ip))
-
-        while recu:
-            #Attend de recevoir un message
-            data, self.address = self.UDPClientSocket.recvfrom(4096)
-            messageVerif = data.decode('utf-8')
-            if (messageVerif == "allo le monde !"):
-            #Confirmation que le message est recu)
-                msg = "MAC recu" 
-                self.UDPClientSocket.sendto(msg.encode('utf-8'), self.address)
-                recu = False
-                #print(recu)
-
-        #fin de config et exemple d'envoie de message par UDP       
+        print(self.UDPClientSocket)
+        self.UDPClientSocket.bind((ip, 4210))
+        print(self.UDPClientSocket)
+        self.address = ('10.240.8.132', 4210)
         msg = "Configuration terminée."
         self.UDPClientSocket.sendto(msg.encode(), self.address)
+        
 
-def initUDP(ip = "192.168.4.1", port= 4210):
-        serverAddressPort = (ip, port)
-        return(serverAddressPort)
-        ###Fonction connecte automatiquement au ESP de Nath
+        #self.x.abort()
 
+
+def get_private_ip():
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as st:
+        st.settimeout(0.0)
+        try:
+            st.connect(('8.8.8.8', 80))
+            ip = st.getsockname()[0]
+        except socket.error:
+            ip = '127.0.0.1'
+    return ip
+       
+     
 
 
 def main():
